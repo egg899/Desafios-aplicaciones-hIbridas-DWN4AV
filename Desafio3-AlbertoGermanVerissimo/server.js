f0591bb0dfd0b236bcfddf449ca894b85fed1e1c
egg899 <germanverissimo5@hotmail.com>
2024-09-03 13:59:59 -0300

    Problemas resueltos

--- Desafio3-AlbertoGermanVerissimo/server.js
+++ Desafio3-AlbertoGermanVerissimo/server.js
@@ -1,12 +1,12 @@
 import http from "http";
-import fs from "fs";
+import { readFile } from "fs/promises";
 import os from "os";
 import path from "path";
 
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     // console.log("url", req.url);
     // console.log("method", req.method);
     // console.log("url", req.headers);
@@ -15,14 +15,19 @@ const server = http.createServer((req, res) => {
 
     switch(req.method) {
         case 'GET':
-            if(req.url === '/alumno') {
+            if(req.url === '/') {
+                // Handle 404 Not Found
+                res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Pagina principal');
+            }
+            else if(req.url === '/alumno') {
                 res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8' });
                 res.end('Alumno: Alberto Germán Verissimo, Comisión:W0D44')
             }//alumno
             else if(req.url === '/info') {
                 const osInfo = {
                     platform:os.platform(),
-                    architecture: os.architecture(),
+                    architecture: os.arch(),
                     cores: os.cpus().length,
                     memory:os.totalmem(),
                     uptime:os.uptime()    
@@ -30,19 +35,20 @@ const server = http.createServer((req, res) => {
                 res.writeHead(200, {'Content-Type':'application/json'});
                 res.end(JSON.stringify(osInfo));
             }//info
-
             else if(req.url === '/static') {
-                const filePath = path.join(__dirname, 'index.html');
-                fs.readFile(filePath, (err, content) => {
-                    if(err){
-                        res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-                        res.end('Error al leer el archivo');
-                    } else {
-                        res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
-                        res.end(content);
-                    }
-                })
-            }//static
+                try {
+                    const data = await readFile('index.html', 'utf-8');
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(data);
+                } catch (err) {
+                    res.statusCode = 500;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Error al leer el archivo');
+                    console.error(err);
+                }
+            }
+            
             else {
                 // Handle 404 Not Found
                 res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
